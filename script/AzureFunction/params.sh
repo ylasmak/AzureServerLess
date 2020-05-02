9e1e87b7cb51ca9426cc5d3b90114512003acaea
Youness Lasmak <ylasmak@sqli.com>
2020-05-02 17:29:38 +0000

    cleaning code

--- script/AzureFunction/params.sh
+++ script/AzureFunction/params.sh
@@ -1,3 +1,7 @@
 weatheralertfunction="weatheralertfunctionapp"
-gitrepo="https://github.com/ylasmak/AzureCli.git"
-token="1907d11627f22e976806c61e94cc41df848f3066"
+gitrepo="https://github.com/ylasmak/AzureServerLess.git"
+token="1dcfd1162ce9008ef527c0d3b795c29ecc08724e"
+
+. ../eventhub/params.sh
+. ../storageaccount/params.sh
+
--- script/AzureFunction/script.sh
+++ script/AzureFunction/script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+. ./params.sh
+
+echo $resource_group_name
 
 storage_account_connection_string=$(az storage account show-connection-string \
                                     --name  $weather_storage_account_name --resource-group \
                                     $resource_group_name --output tsv)
 
 eh_policy_read_primary_connection_string=$(az eventhubs namespace authorization-rule  keys list \
-                                          --resource-group  $resource_group_name  --namespace-name $event_Hubs_namespace \
+                                          --resource-group  $resource_group_name  \
+                                          --namespace-name $event_Hubs_namespace \
                                           --name $readauthorule -o tsv --query primaryConnectionString)
 
 az functionapp deployment source update-token --git-token $token
+
+echo "Create the function app"
 
 az functionapp create --resource-group $resource_group_name \
                          --name $weatheralertfunction \
@@ -18,6 +24,7 @@ az functionapp create --resource-group $resource_group_name \
                          --consumption-plan-location $location \
                          --deployment-source-url \
                          $gitrepo --deployment-source-branch master
+echo "Setup function settings"
 
 az functionapp config appsettings set \
                          --resource-group $resource_group_name \
@@ -27,6 +34,7 @@ az functionapp config appsettings set \
 az functionapp config appsettings set \
                         --resource-group $resource_group_name --name \
                         $weatheralertfunction \
-                        --settings  "nsweatheralerteventeh_readauthorule_EVENTHUB=$eh_policy_read_primary_connection_string"
+                        --settings  "EventHubConnectionString=$eh_policy_read_primary_connection_string"
 
+echo "Strat Fcuntion"
 az functionapp start --resource-group $resource_group_name --name $weatheralertfunction 
